00325e90c41162c62d4871998dd6619e33f744d4
Junyang Ma <specter4mjy@gmail.com>
2016-02-10 01:34:30 +0000

    Finish assignment 1 and update README

---
+++ HelloUCD/HelloUCD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloUCD
+//
+//  Created by Junyang ma on 2/10/16.
+//  Copyright © 2016 Junyang ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
