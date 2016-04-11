f5bc2ce900e4d14555da26ee586a88191c49bf91
Junyang Ma <specter4mjy@gmail.com>
2016-04-11 19:20:36 +0100

    init watchOS Demo

---
+++ ASSIGNMENT/WatchOSDemo/WatchOSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchOSDemo
+//
+//  Created by Junyang ma on 4/11/16.
+//  Copyright © 2016 Junyang ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
