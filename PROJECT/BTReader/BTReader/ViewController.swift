57ac91c854fcc6b8f0fd59d44611fe33d7c8bb91
Junyang Ma <specter4mjy@gmail.com>
2016-04-20 03:17:53 +0100

    iphone can pass the scroll position to iPad

--- PROJECT/BTReader/BTReader/ViewController.swift
+++ PROJECT/BTReader/BTReader/ViewController.swift
@@ -18,12 +18,16 @@ class ViewController: UIViewController,CBPeripheralManagerDelegate, CBCentralMan
     var myCentralManager : CBCentralManager!
     var myPeripheralManager : CBPeripheralManager!
     var myPeripheral : CBPeripheral!
+    var myIphoneScrollingPositionCharacteristic : CBMutableCharacteristic!
     
     @IBOutlet weak var iphoneTV: UITextView! {
         didSet{
             iphoneTV.delegate = self
         }
     }
+    @IBOutlet weak var iPadTV: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -37,6 +41,15 @@ class ViewController: UIViewController,CBPeripheralManagerDelegate, CBCentralMan
         }
     }
     
+    
+    
+    // MARK: iPad - Central Manager
+    
+    private func setupCentral(){
+        myCentralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    
     func centralManagerDidUpdateState(central: CBCentralManager) {
         if central.state == .PoweredOn{
             print("central power on")
@@ -47,13 +60,68 @@ class ViewController: UIViewController,CBPeripheralManagerDelegate, CBCentralMan
         }
     }
     
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        print(advertisementData)
+        myPeripheral = peripheral
+        central.connectPeripheral(peripheral, options: nil)
+        central.stopScan()
+        print(peripheral)
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        print("peripheral connected")
+        peripheral.delegate = self
+        peripheral.discoverServices([myServiceUUID])
+    }
+    
+    // MARK: iPad - Peripheral Delegates
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        for service in peripheral.services! {
+            print(service)
+            peripheral.discoverCharacteristics([myCharacteristiceUUID], forService: service)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        print("discovered Characteristic")
+        for characteristic in service.characteristics!{
+            print(characteristic)
+            // redaing value
+//            peripheral.readvalueforcharacteristic(characteristic)
+            // subscribe value
+            peripheral.setNotifyValue(true, forCharacteristic: characteristic)
+        }
+    }
+    
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateNotificationStateForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        if error != nil {
+            print(" reading subscribed value has error")
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        if let data = characteristic.value {
+            var value : Float = 0
+            data.getBytes(&value, length: sizeof(Float))
+//            print(value)
+            let offsetPoint = CGPoint(x: 0, y: Double(value))
+            iPadTV.setContentOffset(offsetPoint, animated: false)
+        }
+    }
+    
+    // MARK: iPhone - Peripheral Manager
+    private func setupPeripheral(){
+        myPeripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+    }
+    
     func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
         if peripheral.state == .PoweredOn{
             print("peripheral power on")
-            let myData = "specter".dataUsingEncoding(NSUTF8StringEncoding)
-            let myCharacteristic = CBMutableCharacteristic(type: myCharacteristiceUUID, properties: .Read, value: myData, permissions: .Readable)
+            myIphoneScrollingPositionCharacteristic = CBMutableCharacteristic(type: myCharacteristiceUUID, properties: [.Read,.Notify], value: nil, permissions: .Readable)
             let myService = CBMutableService(type: myServiceUUID, primary:true)
-            myService.characteristics = [myCharacteristic]
+            myService.characteristics = [myIphoneScrollingPositionCharacteristic]
             myPeripheralManager.addService(myService)
             let advertisingData : [ String : AnyObject] = [
                 CBAdvertisementDataLocalNameKey : "BTReader",
@@ -79,57 +147,20 @@ class ViewController: UIViewController,CBPeripheralManagerDelegate, CBCentralMan
         }
     }
     
-    private func setupCentral(){
-        myCentralManager = CBCentralManager(delegate: self, queue: nil)
+    func peripheralManager(peripheral: CBPeripheralManager, central: CBCentral, didSubscribeToCharacteristic characteristic: CBCharacteristic) {
+        print(" did subscribe characteristic")
     }
     
-    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
-        print(advertisementData)
-        myPeripheral = peripheral
-        central.connectPeripheral(peripheral, options: nil)
-        central.stopScan()
-        print(peripheral)
-    }
     
-    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
-        print("peripheral connected")
-        peripheral.delegate = self
-        peripheral.discoverServices([myServiceUUID])
-    }
-    
-    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
-        for service in peripheral.services! {
-            print(service)
-            peripheral.discoverCharacteristics([myCharacteristiceUUID], forService: service)
-        }
-    }
-    
-    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
-        print("discovered Characteristic")
-        for characteristic in service.characteristics!{
-            print(characteristic)
-            peripheral.readValueForCharacteristic(characteristic)
-        }
-    }
-    
-    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
-        if let data = characteristic.value {
-            if let stringValue = String(data: data, encoding: NSUTF8StringEncoding){
-                print(stringValue)
-            }
-        }
-    }
-    
-    private func setupPeripheral(){
-        myPeripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-    }
     
     
     @IBAction func iphoneSendCurrentText(sender: UIBarButtonItem) {
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        print(iphoneTV.contentOffset)
+        var floatData = Float(iphoneTV.contentOffset.y)
+        let myData = NSData(bytes: &floatData, length: sizeof(Float))
+        myPeripheralManager.updateValue(myData, forCharacteristic: myIphoneScrollingPositionCharacteristic, onSubscribedCentrals: nil)
     }
     
     override func didReceiveMemoryWarning() {
